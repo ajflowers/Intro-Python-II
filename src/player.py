74e09e4ad7f0fe55574b248440e23e2e40d208c9
Angela Flowers <me@angela.flowers>
2020-02-13 15:37:07 -0500

    added item class, inventory and room items list

--- src/player.py
+++ src/player.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
     def travel(self, direction):
         next_room = getattr(self.current_room, f"{direction}_to")
         if next_room is not None:
